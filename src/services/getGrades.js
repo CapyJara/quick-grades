4439ae6573e6847f90e7ab32a3c396b62b8fe493
UncleJerry <jared.myhrberg@gmail.com>
2019-11-14 11:57:23 -0800

    css and ass fetch fix

--- src/services/getGrades.js
+++ src/services/getGrades.js
@@ -33,7 +33,7 @@ export function getFreshies(apiKey) {
 export function getAsses(kido, apiKey) {
   if(kido.pending.length === 0) return Promise.resolve([{ name: 'No Ungraded Asses', url: null }]);
   const assIds = kido.pending.map(ass => ass.assignmentId).join(',');
-  return fetch(`https://thawing-dusk-78361.herokuapp.com/api/v1/canvas/assignments?ids=${assIds}&student=${kido.id}`, {
+  return fetch(`https://thawing-dusk-78361.herokuapp.com/api/v1/canvas/assignments?ids=${assIds}&studentId=${kido.id}`, {
     headers: {
       'x-api-key': apiKey
     } 

3bbb57b00a1a1f7ea9c8a6a54c533e07a6f234b8
UncleJerry <jared.myhrberg@gmail.com>
2019-11-20 11:40:40 -0800

    removied header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Main from '../containers/Main';
-import Header from './Header';
 import styles from './app.css';
 
 export default function App() {
   return (
     <section className={styles.app}>
-      <Header />
+      <h1>Slightly Quickr Grading</h1>
       <Main />
     </section>
   );

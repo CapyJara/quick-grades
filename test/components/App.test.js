1741c4f3e6c163c5cff81b415edf0d3d6668abe8
UncleJerry <jared.myhrberg@gmail.com>
2019-11-20 15:12:34 -0800

    moved app test

--- test/components/App.test.js
+++ test/components/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import App from '../../src/components/App';
 
 describe('App component', () => {
   it('renders App', () => {

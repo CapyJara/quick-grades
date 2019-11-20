3d195b0c870cd142ff252d25b109f53dfa72828c
UncleJerry <jared.myhrberg@gmail.com>
2019-11-20 13:29:43 -0800

    made link for test asses open to test page

--- src/components/asses/Ass.js
+++ src/components/asses/Ass.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ass.css';
+import { Link } from 'react-router-dom';
 
 function Ass({ ass }) {
   return (
     <li className={styles.ass}>
-      <a href={ass.gradeUrl} target="_blank" rel="noopener noreferrer" >{ass.assignmentName}</a>
+      {ass.gradeUrl ? 
+        <a href={ass.gradeUrl} target="_blank" rel="noopener noreferrer" >{ass.assignmentName}</a> : 
+        <Link to="/ass"   >{ass.assignmentName}</Link>}
     </li>
   );
 }

ea9df71f6d774686e82887a6ce6d84f2ca13aee6
UncleJerry <jared.myhrberg@gmail.com>
2019-11-20 16:01:05 -0800

    window close button for test page

--- src/components/pages/TestAss.js
+++ src/components/pages/TestAss.js
@@ -5,6 +5,7 @@ const TestAss = () => {
     <>
       <h1>Sorry</h1>
       <h3>You cant actually look at students grades if you&quot;re using the &ldquo;TEST&ldquo; api key</h3>
+      <button onClick={() => window.close()}>Close this winda</button>
     </>
   );
 };
